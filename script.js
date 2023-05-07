458063552190c0b9dba42819a34f8c6391672202
Geoff Sweetwood <gsweetwood123@gmail.com>
2023-05-07 12:56:14 -0400

    added multipication function

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const operatorBtns = document.querySelectorAll(`.operator`);
 const inputScreen = document.querySelector(`#input`);
 const topScreen = document.querySelector(`#top`);
 const clearBtn = document.querySelector(`#clear`);
+const equalBtn = document.querySelector(`#equals`);
+let operator;
+let firstNum;
+let secondNum;
 
 numberBtns.forEach(btn => btn.addEventListener(`click`, () => {
     inputScreen.textContent = inputScreen.textContent + btn.textContent;
@@ -14,6 +18,20 @@ clearBtn.addEventListener(`click`, () => {
 })
 
 operatorBtns.forEach(btn => btn.addEventListener(`click`, () => {
-    topScreen.textContent = btn.textContent + inputScreen.textContent;
+    firstNum = +inputScreen.textContent;
+    topScreen.textContent = btn.textContent + `   ` + inputScreen.textContent;
     inputScreen.textContent = ``;
+    operator = btn.textContent;
+    console.log(firstNum);
+    console.log(operator);
 }));
+
+equalBtn.addEventListener(`click`, () => {
+    secondNum = inputScreen.textContent;
+    if (operator = `x`) {
+        inputScreen.textContent = multipy(firstNum, secondNum);
+        topScreen.textContent = ``;
+    }
+});
+
+const multipy = (a, b) => {return a*b};
