61022ed9de5daa2835e0009a1f8871ddcd73dae6
Geoff <gsweetwood123@gmail.com>
2023-05-07 19:23:14 -0400

    added all button functionality except for decimal

--- script.js
+++ script.js
@@ -1,37 +1,85 @@
+// Set variables
 const numberBtns =  document.querySelectorAll(`.number`);
 const operatorBtns = document.querySelectorAll(`.operator`);
 const inputScreen = document.querySelector(`#input`);
 const topScreen = document.querySelector(`#top`);
 const clearBtn = document.querySelector(`#clear`);
 const equalBtn = document.querySelector(`#equals`);
+const decimalBtn = document.querySelector(`#decimal`);
+const percentBtn = document.querySelector(`#percent`);
+const deleteBtn = document.querySelector(`#delete`);
 let operator;
 let firstNum;
 let secondNum;
 
+
+//Button functions
 numberBtns.forEach(btn => btn.addEventListener(`click`, () => {
     inputScreen.textContent = inputScreen.textContent + btn.textContent;
 }));
 
+
+percentBtn.addEventListener(`click`, () => {
+    inputScreen.textContent = (inputScreen.textContent / 100);
+});
+
+deleteBtn.addEventListener(`click`, () => {
+    inputScreen.textContent = inputScreen.textContent.substring(0,inputScreen.textContent.length - 1);
+})
+
 clearBtn.addEventListener(`click`, () => {
     inputScreen.textContent = ``;
     topScreen.textContent = ``;
-})
+});
 
 operatorBtns.forEach(btn => btn.addEventListener(`click`, () => {
     firstNum = +inputScreen.textContent;
-    topScreen.textContent = btn.textContent + `   ` + inputScreen.textContent;
+    topScreen.textContent = inputScreen.textContent + `   ` + btn.textContent;
     inputScreen.textContent = ``;
     operator = btn.textContent;
-    console.log(firstNum);
-    console.log(operator);
 }));
 
 equalBtn.addEventListener(`click`, () => {
     secondNum = inputScreen.textContent;
-    if (operator = `x`) {
-        inputScreen.textContent = multipy(firstNum, secondNum);
-        topScreen.textContent = ``;
-    }
-});
+    switch(operator) {
+        case `x`:
+            inputScreen.textContent = multipy(firstNum, secondNum);
+            topScreen.textContent = ``;
+            break;
+            case `/`:
+                inputScreen.textContent = divide(firstNum, secondNum);
+                topScreen.textContent = ``;
+                break;
+                case `+`:
+            inputScreen.textContent = add(firstNum, secondNum);
+            firstNum = inputScreen.textContent;
+            topScreen.textContent = ``;
+            break;
+            case `-`:
+                inputScreen.textContent = subtract(firstNum, secondNum);
+            firstNum = inputScreen.textContent;
+            topScreen.textContent = ``;
+            break;
+        }
+    });
 
-const multipy = (a, b) => {return a*b};
+
+//Operator functions
+const multipy = (a, b) => {
+    return a*b
+};
+
+const divide = (a,b) => {
+    if (b === `0`) {
+        return `Stop it.`;
+    }
+        return a/b;
+};
+
+const add = (a,b) => {
+    return a + b;
+};
+
+const subtract = (a,b) => {
+    return a - b;
+};
